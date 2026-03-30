7e55ee54018e66bb55c7382cdb26d675e0cf9e5f
quintans <vitorito20476@gmail.com>
2026-03-30 01:26:01 -0300

    feat: creating a Point of Memory (POM) for the progress bar page and developing acceptance criteria for this page.

--- web/cypress/e2e/widgets/widgets.spec.cy.js
+++ web/cypress/e2e/widgets/widgets.spec.cy.js
@@ -1,4 +1,10 @@
 import WidgetsPage from "../../support/pages/widgets/widgetsPage";
+import ProgressBarPage from "../../support/pages/widgets/progressBar/progressBarPage";
+
+// Prevents Cypress from failing the test if external scripts/website ads return an error.
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
 
 describe('Widgets Tests', () => {
     beforeEach(() => {
@@ -9,6 +15,20 @@ describe('Widgets Tests', () => {
         it('should access the Widgets Page', () => {
             WidgetsPage.accessWidgetsPage();
             WidgetsPage.validateWidgetsPage();
+        });
+
+        it('should interact with the Progress Bar according to Acceptance Criteria', () => {
+            WidgetsPage.accessWidgetsPage();
+            ProgressBarPage.accessProgressBarPage();
+            ProgressBarPage.validateProgressBarPage();
+
+            ProgressBarPage.startProgressBar();
+
+            ProgressBarPage.stopBefore(25);
+
+            ProgressBarPage.validateProgress(25);
+
+            ProgressBarPage.startAndResetAt100();
         });
     });
 });
--- web/cypress/support/pages/widgets/widgetsElements.js
+++ web/cypress/support/pages/widgets/widgetsElements.js
@@ -1,5 +1,6 @@
 const WidgetsPage_ELEMENTS = {
     widgetsCard: '.category-cards a[href="/widgets"]',
+    progressBarSubMenu: '#item-4 > a[href="/progress-bar"]',
 }
 
 export default WidgetsPage_ELEMENTS;
